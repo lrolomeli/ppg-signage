50a3f31dcffcd9dfe5075caea8b69a8f76dabd9d
luisrlppg <luis.lomeli@plasticosplasa.com>
2025-01-14 11:49:08 -0600

    Update refresh_signage.sh

--- autoscripts/refresh_signage.sh
+++ autoscripts/refresh_signage.sh
@@ -1,4 +1,4 @@
 export DISPLAY=:0
-xdotool mousemove 0 0
+# xdotool mousemove 0 0
 # xdotool search --onlyvisible --class firefox windowactivate key F5
 xdotool search --onlyvisible --class chromium windowactivate key F5 F5
