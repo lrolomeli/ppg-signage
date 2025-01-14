837dabfb7de039e896f1bf3ac4eafdae744adaa4
luisrlppg <luis.lomeli@plasticosplasa.com>
2025-01-14 11:52:52 -0600

    Update start_signage.sh

--- autoscripts/start_signage.sh
+++ autoscripts/start_signage.sh
@@ -3,5 +3,5 @@ export DISPLAY=:0
 URL="https://docs.google.com/presentation/d/e/2PACX-1vRSuvqnKKk2evC1_hLz-dRnSZ4KXKb4jSaniehgcObKqCq32UEaqesFzHUe4t81Mdfh0Sz-UtU1fu-p/pub?start=true&loop=true&delayms=10000"
 
 # firefox --kiosk "$URL"
-chromium-browser --noerrdialogs --disable-session-crashed-bubble --disable-infobars --start-fullscreen "$URL"
+chromium-browser --start-fullscreen "$URL"
 
