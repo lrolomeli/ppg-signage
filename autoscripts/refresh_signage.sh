9da55c69d2b6e94268e7fc90e25f09d2091361f0
Luis Lomeli <luis.lomeli@plasticosplasa.com>
2025-02-01 08:44:30 -0600

    adjustments for having the tv stable

--- autoscripts/refresh_signage.sh
+++ autoscripts/refresh_signage.sh
@@ -2,3 +2,6 @@ export DISPLAY=:0
 # xdotool mousemove 0 0
 # xdotool search --onlyvisible --class firefox windowactivate key F5
 xdotool search --onlyvisible --class chromium windowactivate key F5 F5
+date
+echo 'ping 0.0.0.0' | cec-client -s -d 1
+echo '---------------------------------'
--- autoscripts/start_signage.sh
+++ autoscripts/start_signage.sh
@@ -1,7 +1,7 @@
 export DISPLAY=:0
 
 URL="https://docs.google.com/presentation/d/e/2PACX-1vRSuvqnKKk2evC1_hLz-dRnSZ4KXKb4jSaniehgcObKqCq32UEaqesFzHUe4t81Mdfh0Sz-UtU1fu-p/pub?start=true&loop=true&delayms=10000"
-
+#El comando "as" en cec-client significa "Active Source" (Fuente Activa). Esto le indica al dispositivo conectado por HDMI (como un televisor) que la Raspberry Pi u otro dispositivo con cec-client debe ser tratado como la fuente activa de video.
+echo "as" | cec-client -s -d 1
 # firefox --kiosk "$URL"
 chromium-browser --start-fullscreen "$URL"
-
