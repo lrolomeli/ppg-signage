f0f05f5cadffe3af20c4d6fb1a9545788502ead0
luis lomeli <lro.lomeli@gmail.com>
2025-03-13 11:46:22 -0600

    adding execution permission to scripts, disabling screen protector

--- setup.sh
+++ setup.sh
@@ -25,11 +25,20 @@ echo "Carpeta /var/log/autoscripts/ creada."
 echo "Moviendo autoscripts a ~/Documents/"
 mv autoscripts ~/Documents/
 
+# Otorgar permisos de ejecución a todos los archivos dentro de autoscripts
+echo "Otorgando permisos de ejecución a archivos en autoscripts..."
+chmod -R +x ~/Documents/autoscripts/
+echo "Permisos asignados."
+
 # Copiar el archivo signage-fs.desktop a ~/.config/autostart/
 echo "Copiando signage-fs.desktop a autostart..."
 mkdir -p ~/.config/autostart/
 mv ~/Documents/autoscripts/signage-fs.desktop ~/.config/autostart/
-echo "Archivo signage-fs.desktop copiado a ~/.config/autostart/"
+chmod +x ~/.config/autostart/signage-fs.desktop
+echo "Archivo signage-fs.desktop copiado y con permisos de ejecución."
+
+# Asegurar que crontab.txt tenga una nueva línea al final
+echo "" >> ~/Documents/autoscripts/crontab.txt
 
 # Copiar el contenido de crontab.txt a crontab
 echo "Configurando crontab..."
@@ -40,3 +49,10 @@ echo "Crontab actualizado correctamente."
 echo "Habilitando X11..."
 sudo raspi-config nonint do_wayland W1
 echo "X11 habilitado. Es recomendable reiniciar el Raspberry Pi."
+
+# Deshabilitar protector de pantalla y evitar que la pantalla se apague
+echo "Deshabilitando protector de pantalla y suspensión de pantalla..."
+xset s off
+xset -dpms
+xset s noblank
+echo "Protector de pantalla deshabilitado y la pantalla no se suspenderá."
